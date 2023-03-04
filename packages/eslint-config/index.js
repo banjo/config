d6704ec348035d01153e4865bb8172ded09103eb
Anton Ödman <anton.odman@gmail.com>
2023-03-04 23:56:35 +0100

    chore: test override

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -22,6 +22,16 @@ module.export = {
         "unused-imports",
         "json",
     ],
+    overrides: [
+        {
+            parserOptions: {
+                tsconfigRootDir: process.cwd(),
+                project: ["./tsconfig.json"],
+            },
+            parser: "@typescript-eslint/parser",
+            files: ["*.ts", "*.tsx", "*.mts", "*.cts"],
+        },
+    ],
     parserOptions: {
         ecmaVersion: "latest",
         sourceType: "module",
