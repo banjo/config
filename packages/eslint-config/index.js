407dc846f8a7b75c9608f58dd0422d7c8e4f0458
Anton Ödman <anton.odman@gmail.com>
2024-09-15 21:52:40 +0200

    chore: add eslint prettier

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -2,5 +2,11 @@ import tseslint from "typescript-eslint";
 import { defaultConfig } from "./default-config.js";
 import { reactConfig } from "./react.js";
 import { typescriptConfig } from "./typescript.js";
+import prettierConfig from "@banjoanton/eslint-config-prettier";
 
-export default tseslint.config(...defaultConfig, ...reactConfig, ...typescriptConfig);
+export default tseslint.config(
+    ...defaultConfig,
+    ...reactConfig,
+    ...typescriptConfig,
+    ...prettierConfig
+);
