43ec3046b9d4b314a4d2265698cebc62b0e54ea3
Anton Ödman <anton.odman@gmail.com>
2024-09-16 15:56:38 +0200

    chore: update path

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -1,5 +1,5 @@
 import tseslint from "typescript-eslint";
-import defaultConfig from "./js.js";
+import defaultConfig from "./javascript.js";
 import reactConfig from "./react.js";
 import typescriptConfig from "./typescript.js";
 import prettierConfig from "@banjoanton/eslint-config-prettier";
